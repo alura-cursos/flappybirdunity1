d9f33eeb899a374b4e5b64cd589799527db9609b
Ricardo <dones.bugan@gmail.com>
2018-02-14 15:18:20 -0200

    projeto final aula 1

---
+++ Assets/Scripts/Aviao.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour {
+    Rigidbody2D fisica;
+
+    private void Awake()
+    {
+        this.fisica = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.Impulsionar();
+        }
+		
+	}
+
+    private void Impulsionar()
+    {
+        this.fisica.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+}
